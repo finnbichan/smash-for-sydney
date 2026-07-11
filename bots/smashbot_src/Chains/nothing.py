302354d27c561169ed783951bfa09b707342b437
Finn Bichan <finnbichan@gmail.com>
2026-07-11 14:21:01 +1000

    removing smashbot

--- bots/smashbot_src/Chains/nothing.py
+++
@@ -1,7 +0,0 @@
-import melee
-from Chains.chain import Chain
-
-class Nothing(Chain):
-    def step(self, gamestate, smashbot_state, opponent_state):
-        self.controller.empty_input()
-        self.interruptible = True
